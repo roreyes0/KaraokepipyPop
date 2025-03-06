5867312a4ab131657ce27d89e856654a5337fa2f
roreyes0 <rociodelosangelesgallaghermarr@gmail.com>
2025-03-05 21:49:42 -0600

    added style :lipstick:

--- main.js
+++ main.js
@@ -11,7 +11,10 @@ function addSong(songName,artistName,songUrl){
             <a href="${songUrl}" target="_blank">Reproducir</a>
             <button class="delete-btn">Eliminar</button>
     `;
-
+    const deleteButton = listSong.querySelector('.delete-btn');
+    deleteButton.addEventListener('click', () => {
+        playList.removeChild(listSong);
+    });
 
     //agregar al html
     playList.appendChild(listSong);
